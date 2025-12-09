fef6b8f74dcacaf0860024a336ff5189286cef7b
Nightfury <goldfury.exe@gmail.com>
2025-12-09 15:14:59 +0000

    halfway point not everything is used and a lot of test lines are still there but i need to commit now so

---
+++ projet_mongo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <helloworld msg="YOU DID IT!!!"/>
+</template>
+
+<script>
+    import Floatingwindow from './components/Floatingwindow.vue';
+    export default {
+        components: {
+            Floatingwindow
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: sans-serif;
+    }
+</style>
---
+++ projet_mongo/src/components/Floatingwindow.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="floating-window">
+        <p>This is pretty {{ fire }}</p>
+    </div>
+</template>
+
+<script>
+
+module.exports = {
+    data: function() {
+        return {
+            fire: "AAAAAAAAAAAAAAAAA",
+        }
+    },
+    methods: {
+        helloworld() {
+            return "WEEEEEEEEEEEEEEE";
+        }
+    },
+    components: {}
+}
+
+</script>
+
+<style>
+    div {
+        width: 50%;
+        height: 50%;
+        border: 2px solid black;
+    }
+</style>
